4fa12190e8f2bf58d04a38801b1cfce2629c7167
Tom Zhang <tommunistzhangster@gmail.com>
2019-02-18 16:59:28 +0000

    yay

--- static/homepage.js
+++ static/homepage.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    var navBar = document.getElementById("navBar");
-    navBar.style.height = 0;
-
-    document.getElementById("navButton").onclick = function() {
-
-        if (navBar.height === "0px") {
-            navBar.style.height = "70px";
-        } else {
-            navBar.style.height = "0px";
-        }
-    }
+    // var navBar = document.getElementById("navBar");
+    // navBar.style.height = 0;
+    //
+    // document.getElementById("navButton").onclick = function() {
+    //
+    //     if (navBar.height === "0px") {
+    //         navBar.style.height = "70px";
+    //     } else {
+    //         navBar.style.height = "0px";
+    //     }
+    // }
 
 });
